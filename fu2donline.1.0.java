7bea135a1d39c0dc906d5e52e8f25745996ae3fe
985434801 <47844970+985434801@users.noreply.github.com>
2019-06-04 19:30:18 +0800

    Add files via upload

---
+++ fu2donline.1.0.java
@@ -0,0 +1,13 @@
+{
+    "ret": 200, 
+    "data": {
+        "code": 0, 
+        "msg": "", 
+        "info": [
+            {
+                "data": "Omn3CKSTRJ3TD5DdGAyaGPS/ABJj1VswjIBEdw0wT/qNuOS+cnvKEz3zA+5ZBz+8ur5QiszuZZV8WuGoWF6AEJYaE4ZxbYPLbfI+CX9dj/UvCXis29WT9PkY071xyqFyKS5CyWZ7yL255jiFuTzJ6S01fC7i1YkSXYiJp03ceZo="
+            }
+        ]
+    }, 
+    "msg": ""
+}
